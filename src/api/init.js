69387ea93a97a47c55148cbcb2217a3e7be0fff4
R Surahutomo Aziz Pradana <surahutomo.pradana@bukalapak.com>
2022-10-03 01:22:16 +0700

    update firebase init

--- src/api/init.js
+++ src/api/init.js
@@ -1,23 +1,30 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+import { getAnalytics } from "firebase/analytics";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD6W-8tFlIFqYDaGkW2MNixQ9DacKdV8Cg",
-  authDomain: "gdg-jakarta-community.firebaseapp.com",
-  projectId: "gdg-jakarta-community",
-  storageBucket: "gdg-jakarta-community.appspot.com",
-  messagingSenderId: "665303790450",
-  appId: "1:665303790450:web:2c9868560d8543b17623eb",
-  measurementId: "G-EJ7N8ET7RX",
+  apiKey: "AIzaSyD-J-BL09PK_DxIbjpERcN4y_EpPruhY0w",
+  authDomain: "indonesia-firebase-playground.firebaseapp.com",
+  projectId: "indonesia-firebase-playground",
+  storageBucket: "indonesia-firebase-playground.appspot.com",
+  messagingSenderId: "109641815905",
+  appId: "1:109641815905:web:ed6d0d7fdd5ea479dbd720",
+  measurementId: "G-Q5R35B65T2"
 };
 
 const initializeFirestore = () => {
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
+  initializeAnalytics(app)
 
   // Initialize Cloud Firestore and get a reference to the service
   return getFirestore(app);
 };
+
+const initializeAnalytics = (app) => {
+  getAnalytics(app);
+}
 
 const initilizedFirebase = {
   initializeFirestore
