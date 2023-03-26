3b64f3963f0dac8a6ce17da35f74dadce7a60461
surahutomo.pradana@bukalapak.com <surahutomo.pradana@bukalapak.com>
2023-03-26 15:01:58 +0700

    revamp test cases

--- test/security-rules.test.js
+++ test/security-rules.test.js
@@ -1,40 +1,123 @@
-const {
-  initializeTestEnvironment,
-  assertSucceeds,
-  assertFails,
-} = require("@firebase/rules-unit-testing");
-
-const { doc, setDoc } = require("firebase/firestore");
+const { initializeTestEnvironment, assertSucceeds, assertFails } = require("@firebase/rules-unit-testing");
+const { doc, getDoc, setDoc } = require("firebase/firestore");
 
 const PROJECT_ID = "indonesia-firebase-playground";
+
 let firebase;
 
-const getFirebase = async (auth) => {
-  const config = {
-    projectId: PROJECT_ID,
-    firestore: {
-      host: "localhost",
-      port: 8000,
-    },
-  };
-  return await initializeTestEnvironment(config);
-};
+const getFirebase = async () => {
+    return await initializeTestEnvironment({
+        projectId: PROJECT_ID,
+        firestore: {
+            host: "localhost",
+            port: 8000
+        }
+    });
+}
 
-describe("Security Rules", () => {
-  it("should initialize firebase without authentication successfully", async () => {
-    firebase = await getFirebase(null);
-    assertSucceeds(firebase);
-  });
+const getFirestore = (context) => {
+    return context.firestore();
+}
 
-  it("should not be able to write members data if no authentication provided", async () => {
-    firebase = await getFirebase(null);
-    const membersToBeAdded = {
-      name: "public member",
-    };
-    const membersPath = "members/some-id";
-    const firestore = firebase.unauthenticatedContext().firestore();
-    const result = setDoc(doc(firestore, membersPath), membersToBeAdded);
+describe("Members", () => {
+    beforeEach(async () => {
+        firebase = await getFirebase();
+    })
 
-    assertFails(result);
-  });
+    it("should able to see members profile data", async () => {
+        const auth = firebase.unauthenticatedContext();
+        const firestore = getFirestore(auth);
+        const memberRef = "members/userId";
+
+        const snapshot = getDoc(doc(firestore, memberRef));
+
+        assertSucceeds(snapshot);
+    });
+
+    it("should not be able write member data", async () => {
+        const auth = firebase.unauthenticatedContext();
+        const firestore = getFirestore(auth);
+        const memberRef = "members/userId";
+
+        await assertFails(setDoc(doc(firestore, memberRef), {}));
+    });
+
+    it("should be able to write data if the user is authenticated", async () => {
+        const auth = firebase.authenticatedContext('adminId');
+        const firestore = getFirestore(auth);
+        const memberRef = "members/userId";
+        const memberData = {
+            email: "some email"
+        }
+
+        await assertSucceeds(setDoc(doc(firestore, memberRef), memberData));
+    });
+
+    it("should not be able to write data if the user is not authenticated", async () => {
+        const auth = firebase.unauthenticatedContext();
+        const firestore = getFirestore(auth);
+        const memberRef = "members/userId";
+        const memberData = {
+            name: "some name"
+        }
+
+        await assertFails(setDoc(doc(firestore, memberRef), memberData));
+    });
+
+    it("should be able read document with a correct user id", async () => {
+        const auth = firebase.authenticatedContext('userId');
+        const firestore = getFirestore(auth);
+        const memberRef = "members/userId";
+
+        await assertSucceeds(getDoc(doc(firestore, memberRef)));
+    });
+
+    it("should not be able read document with an incorrect user id", async () => {
+        const auth = firebase.authenticatedContext('wrongUserId');
+        const firestore = getFirestore(auth);
+        const memberRef = "members/userId";
+
+        await assertFails(getDoc(doc(firestore, memberRef)));
+    });
+})
+
+describe("Admin", () => {
+    it("should be able to write or update members if the role is admin", async () => {
+        const auth = firebase.authenticatedContext('adminId');
+        const firestore = getFirestore(auth);
+        const memberRef = "members/memberId";
+        const memberData = {
+            email: "some email"
+        }
+
+        await assertSucceeds(setDoc(doc(firestore, memberRef), memberData));
+    });
+
+    it("should not be able to write or update members if the role is not admin", async () => {
+        const auth = firebase.authenticatedContext('memberId');
+        const firestore = getFirestore(auth);
+        const memberRef = "members/memberId";
+
+        await assertFails(setDoc(doc(firestore, memberRef), {}));
+    });
+
+    it("should not be able to write members document if there is no email field", async () => {
+        const auth = firebase.authenticatedContext('adminId');
+        const firestore = getFirestore(auth);
+        const memberRef = "members/memberId";
+        const memberData = {}
+
+        await assertFails(setDoc(doc(firestore, memberRef), memberData));
+    })
+
+    it("should be able to write members document if there is email field", async () => {
+        const auth = firebase.authenticatedContext('adminId');
+        const firestore = getFirestore(auth);
+        const memberRef = "members/memberId";
+        const memberData = {
+            email: "some email"
+        }
+
+        await assertSucceeds(setDoc(doc(firestore, memberRef), memberData));
+    })
 });
